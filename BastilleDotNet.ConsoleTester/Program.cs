f80b6015fd942c1e1d075a49d9d1c0bc5010574a
Craig Larsen <LarsenC@LCS.com>
2024-08-07 20:11:42 -0700

    added console tester

---
+++ BastilleDotNet.ConsoleTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BastilleDotNet.Models;
+using BastilleDotNet.Services;
+
+namespace BastilleDotNet.ConsoleTestermespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = BastilleDotNet.Services.ContainerService.ListContainers();
+            foreach(ContainerInfo ci in results)
+            {
+                Console.WriteLine(ci.Name + " " + ci.Status + " " + ci.IPAddress);
+            }
+            
+        }
+    }
+}
